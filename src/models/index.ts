15d889fca1e2d63d62979087e4423ec587645b4c
Simone Marcato <simone@hevelop.com>
2024-06-04 14:41:06 +0200

    Generated client for service: tokentracker with version: 1.0.0 in language: typescript

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ProtobufAny';
+export * from './ProtobufNullValue';
+export * from './RpcStatus';
+export * from './StripeEventData';
+export * from './StripeEventRequest';
+export * from './TokenTrackerAdjustTokenBalanceRequest';
+export * from './TokentrackerAdjustTokenBalanceResponse';
+export * from './TokentrackerGetTokenBalanceResponse';
+export * from './TokentrackerStripeWebhookRequest';
